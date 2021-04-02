89e1f8f964c4c492af3019467e1df0193d8b351a
wolf-22 <kanino@web.de>
2021-04-03 01:07:57 +0200

    super sunny added

--- WeatherForecastService.cs
+++ WeatherForecastService.cs
@@ -8,7 +8,7 @@ namespace HiddenVilla_Server.Data
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "super sunny"
         };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
