6ca8505f59e54d7cd10ec0fb2aa8ae9d0028f79e
wolf-22 <kanino@web.de>
2021-07-16 01:47:22 +0200

    class City added

---
+++ City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class City
+{
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int IdCountry { get; set; }
+
+    public long Popularion { get; set; }
+
+    public double Area { get; set; }
+
+    public City()
+	{
+	}
+}
